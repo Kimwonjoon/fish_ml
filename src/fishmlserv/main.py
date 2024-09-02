974b5e56c9f3ba85678faf1b0760fe799718de31
Kimwonjoon <kimpass189@gmail.com>
2024-09-02 12:15:12 +0900

    maybe done load model

--- src/fishmlserv/main.py
+++ src/fishmlserv/main.py
@@ -15,7 +15,7 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.get("/fish")
-def fish(length: float, weight: float):
+def fish(length: float = 50, weight: float = 50):
     """
     물고기의 종류 판별기
 
@@ -26,10 +26,10 @@ def fish(length: float, weight: float):
     Returns:  
         dict: 물고기 종류를 담은 딕셔너리  
     """
-    model_path = get_model_path
-#    with open(model_path, 'rb') as f:
-#        fish_model = pickle.load(f)
-    fish_model = get_model_path
+    model_path = get_model_path()
+    #print(model_path)
+    with open(model_path, 'rb') as f:
+        fish_model = pickle.load(f)
     fish_class = fish_model.predict([[length, weight]])
     #fish_class = "몰라"
     if fish_class == 0:
--- src/fishmlserv/model/manager.py
+++ src/fishmlserv/model/manager.py
@@ -1,9 +1,9 @@
 import os
-import pickle
+#import pickle
 def get_model_path():
     path = os.path.dirname(os.path.abspath(__file__))
     model_path = path + "/model.pkl"
-    with open(model_path, 'rb') as f:
-        fish_model = pickle.load(f)
-    return fish_model
+#    with open(model_path, 'rb') as f:
+#        fish_model = pickle.load(f)
+    return model_path
 #print(get_model_path())
